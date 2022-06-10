63844911e47160f2ff3cc20131d6d09c2482516a
Prakash Yogi <yogipra2003@gmail.com>
2022-06-10 15:23:16 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            monsters: [],
+            searchTerm: "",
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState(
+                    () => {
+                        return {
+                            monsters: data,
+                        };
+                    },
+                    () => {
+                        console.log(this.state.monsters);
+                    }
+                );
+            });
+    }
+
+    onSearchChange = (e) => {
+        console.log(e.target.value);
+        const monsterName = e.target.value;
+        this.setState({ searchTerm: monsterName.toLowerCase() });
+    };
+
+    render() {
+        const { monsters, searchTerm } = this.state;
+        const { onSearchChange } = this;
+        const filteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(searchTerm);
+        });
+        return (
+            <div className="App">
+                <input
+                    type="search"
+                    placeholder="Enter name"
+                    className="search-bar"
+                    onChange={onSearchChange}
+                />
+                {filteredMonsters.map((monster) => (
+                    <h1 key={monster.id}>{monster.name}</h1>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
